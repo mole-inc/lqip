df1f813fedd2ba77f302bd18f5004f8c8762b003
Haruaki OTAKE <aaharu@hotmail.com>
2020-04-02 23:06:41 +0900

    test: add Buffer testcases

--- test/lqip.test.ts
+++ test/lqip.test.ts
@@ -1,4 +1,5 @@
 import lqip from "../src/index";
+import fs from "fs";
 
 describe("lqip base64 function", () => {
   it("[sharp] should reject unkown or unsupported file formats", () => {
@@ -7,10 +8,17 @@ describe("lqip base64 function", () => {
     return expect(lqipped).rejects.toThrow("Input file is missing or of an unsupported image format lqip");
   });
 
-  it("[sharp] should generate a valid base64", () => {
+  it("[sharp] should generate a valid base64 from filepath", () => {
     const file = `${__dirname}/img/riding-a-bike.jpg`;
     const expectedBase64 = "data:image/jpeg;base64,/9j/";
     const lqipped = lqip.base64(file);
+    return expect(lqipped).resolves.toContain(expectedBase64);
+  });
+
+  it("[sharp] should generate a valid base64 from Buffer", () => {
+    const file = `${__dirname}/img/riding-a-bike.jpg`;
+    const expectedBase64 = "data:image/jpeg;base64,/9j/";
+    const lqipped = lqip.base64(fs.readFileSync(file));
     return expect(lqipped).resolves.toContain(expectedBase64);
   });
 
@@ -20,10 +28,17 @@ describe("lqip base64 function", () => {
     return expect(lqipped).rejects.toThrow("Input file is missing or of an unsupported image format lqip");
   });
 
-  it("[jimp] should generate a valid base64", () => {
+  it("[jimp] should generate a valid base64 from filepath", () => {
     const file = `${__dirname}/img/riding-a-bike.jpg`;
     const expectedBase64 = "data:image/jpeg;base64,/9j/";
     const lqipped = lqip.base64(file, true);
+    return expect(lqipped).resolves.toContain(expectedBase64);
+  });
+
+  it("[jimp] should generate a valid base64 from Buffer", () => {
+    const file = `${__dirname}/img/riding-a-bike.jpg`;
+    const expectedBase64 = "data:image/jpeg;base64,/9j/";
+    const lqipped = lqip.base64(fs.readFileSync(file), true);
     return expect(lqipped).resolves.toContain(expectedBase64);
   });
 });
